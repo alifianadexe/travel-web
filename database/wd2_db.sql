fbbbb2929311d6dc631a1c2d43f56c083ff803b8
rosamelia <alifianadexe@gmail.com>
2022-06-29 20:36:50 +0700

    change status

--- database/wd2_db.sql
+++ database/wd2_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2022 at 05:30 PM
+-- Generation Time: Jun 29, 2022 at 03:34 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
