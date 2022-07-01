f7c4cf60d96c4d1b441dadeb8412d5f4d383d8c9
alifianadexe <alifianadexe@gmail.com>
2022-07-01 02:02:44 +0000

    config database change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/MVC-OnlineShop/';
+$config['base_url'] = 'http://95.216.203.52/travel-web/';
 
 /*
 |--------------------------------------------------------------------------
